8c75768008f94a849a9c86cfec97926b30e14b4b
mathieu bernard <mathieu.bernard444@gmail.com>
2019-02-26 15:43:56 +0100

    model position ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_141741) do
+ActiveRecord::Schema.define(version: 2019_02_26_144249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "talents", force: :cascade do |t|
     t.string "email", default: "", null: false
