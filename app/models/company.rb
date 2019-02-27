874b6a50c8b41dadc22552f441c8f08a920c65d3
Romain Bertrand <bertrand_romain@live.fr>
2019-02-27 16:04:23 +0100

    seed finished

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,8 @@ class Company < ApplicationRecord
   has_many :events
   has_many :job_offers
   has_many :company_values
+
+  validates :logo, presence: true
+
+  mount_uploader :logo, LogoUploader
 end
