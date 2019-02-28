fa8ff91b285b11989079702219d84d88997d8621
Nora Lasri <nora.lasry@gmail.com>
2019-02-28 12:17:30 +0100

    job

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   validates :category, presence: true
-  has_many :events
-  has_many :job_offers
+  has_many :events, dependent: :destroy
+  has_many :job_offers, dependent: :destroy
   has_many :company_values
 end
