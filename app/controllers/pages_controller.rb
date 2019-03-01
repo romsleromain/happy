966aecff4c23ae33e5e5992ed4bcdf7c4255730b
Philippe Larroque <larroquephilippe@gmail.com>
2019-03-01 16:02:20 +0100

    matchlogic done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,11 +7,7 @@ class PagesController < ApplicationController
   def connexion
   end
 
-<<<<<<< HEAD
-  def feed
-=======
   def sandbox
->>>>>>> 90b7efdd7ad0aac85dca1c1295dcd77202b29526
   end
 
 end
