222c2148cc3c0f590e6d8150de7734b772d2d855
mathieu bernard <mathieu.bernard444@gmail.com>
2019-03-04 20:49:28 +0100

    home ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import "../plugins/values_choice";
 
 import { follow } from "../components/init-btn-follow.js";
-
-
 follow();
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
