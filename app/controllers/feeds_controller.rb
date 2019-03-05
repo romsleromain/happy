20c03e2c023369a720a82b51678377fbc04fe976
Philippe Larroque <larroquephilippe@gmail.com>
2019-03-05 10:45:10 +0100

    scroll added

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,4 +2,17 @@ class FeedsController < ApplicationController
   def index
     @feed = Company.all
   end
+
+  def follow
+    @company = Company.find(params[:id])
+    current_talent.follow(@company)
+    redirect_to feeds_index_path
+  end
+
+  def unfollow
+    @company = Company.find(params[:id])
+    current_talent.stop_following(@company)
+    redirect_to feeds_index_path
+  end
+
 end
