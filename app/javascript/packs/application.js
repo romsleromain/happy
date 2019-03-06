41217f149e764ed93ebd17bdf3fddb7209ed57a4
Romain Bertrand <bertrand_romain@live.fr>
2019-03-06 17:58:26 +0100

    form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,44 @@ if (document.getElementById('nextstep')) {
 }
 
 import { initSweetalert } from '../plugins/init_sweetalert';
+import { initSweetalert2 } from '../plugins/init_sweetalert';
+import { contactManager } from '../plugins/init_sweetalert';
 
 initSweetalert('#sweet-alert-demo', {
-  title: "A nice alert",
-  text: "This is a great alert, isn't it?",
+  title: "Want to contact?",
+  text: "Send a direct message to the manager",
   icon: "success"
+});
+
+
+initSweetalert2('#sweet-alert-demo2', {
+  title: "Want to contact?",
+  text: "Send a direct message to the manager",
+  icon: "success"
+}, (value) => {
+  if (value) {
+    swal("Hello world!");
+  }
+});
+
+contactManager('#contact', {
+  title: "Contact manager ?",
+  text: "Send a direct message to the manager",
+  content: {
+    element: "input",
+    attributes: {
+      placeholder: "Hello, your company is amazing, please hire me !",
+    },
+  },
+  buttons: {
+    cancel: "exit",
+    text: "Send my message!",
+    closeModal: false,
+  }
+}, (value) => {
+  if (value) {
+    swal("Your message has been sent !", {
+      icon: "success"
+    });
+  }
 });
--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,5 +1,15 @@
 import swal from 'sweetalert';
 
+const initSweetalert2 = (selector, options = {}, callback = () => {}) => {
+  const swalButton = document.querySelector(selector);
+  if (swalButton) { // protect other pages
+    swalButton.addEventListener('click', () => {
+      swal(options).then(callback);
+    });
+  }
+};
+
+
 const initSweetalert = (selector, options = {}) => {
   const swalButton = document.querySelector(selector);
   if (swalButton) { // protect other pages
@@ -10,4 +20,15 @@ const initSweetalert = (selector, options = {}) => {
 };
 
 
+const contactManager = (selector, options = {}, callback = () => {}) => {
+  const swalButton = document.querySelector(selector);
+  if (swalButton) { // protect other pages
+    swalButton.addEventListener('click', () => {
+      swal(options).then(callback);
+    });
+  }
+};
+
 export { initSweetalert };
+export { initSweetalert2 };
+export { contactManager };
